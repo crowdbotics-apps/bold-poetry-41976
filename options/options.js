58eae833ea3fe5b60a17dfed82dcb8cf46d1dfba
Crowdbotics <team@crowdbotics.com>
2023-07-07 14:54:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bold-poetry-41976",
+  url: "https://bold-poetry-41976.botics.co",
+  api: "https://bold-poetry-41976.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
